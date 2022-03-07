7912008d64a6b2c5ae05427f5c78826c23855c26
phan-ba-duy <phan-ba-duy@com-way.co.jp>
2022-03-07 15:13:15 +0900

    Resizable added

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -7,17 +7,17 @@ const PictureList = [
   {
     id: 1,
     url:
-      "https://yt3.ggpht.com/ytc/AAUvwnjOQiXUsXYMs8lwrd4litEEqXry1-atqJavJJ09=s900-c-k-c0x00ffffff-no-rj",
+      "https://picsum.photos/id/200/150",
   },
   {
     id: 2,
     url:
-      "https://media-exp1.licdn.com/dms/image/C4D03AQExheo0sff_yQ/profile-displayphoto-shrink_200_200/0/1590072898568?e=1630540800&v=beta&t=_W-gWZewSBYQ-UAjpGvR8h_8Vvo202IHQQissbK2aKc",
+      "https://picsum.photos/id/237/150",
   },
   {
     id: 3,
     url:
-      "https://yt3.ggpht.com/pe57RF1GZibOWeZ9GwRWbjnLDCK2EEAeQ3u4iMAFNeaz-PN9uSsg1p2p32TZUedNnrUhKfoOuMM=s900-c-k-c0x00ffffff-no-rj",
+      "https://picsum.photos/id/250/150",
   },
 ];
 
--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,7 +1,11 @@
-import React from "react";
+import { Resizable } from "re-resizable";
+import React, { useState } from "react";
 import { useDrag } from "react-dnd";
+import { RiDeleteBinLine } from 'react-icons/ri'
 
 function Picture({ id, url }) {
+  const [isShowDelBtn, setIsShowDelBtn] = useState(false)
+
   const [{ isDragging }, drag] = useDrag(() => ({
     type: "image",
     item: { id: id },
@@ -9,13 +13,49 @@ function Picture({ id, url }) {
       isDragging: !!monitor.isDragging(),
     }),
   }));
+
+  function ResizeCallback(event, direction, refToElement, delta) {
+    console.log(delta)
+    console.log(refToElement)
+  }
+
+  const handleMouseOver = (e) => {
+    console.log(e.target.src)
+    setIsShowDelBtn(true);
+  }
+
+  const handleMouseOut = (e) => {
+    console.log(e.target.src)
+    setIsShowDelBtn(false);
+  }
+  
   return (
-    <img
-      ref={drag}
-      src={url}
-      width="150px"
-      style={{ border: isDragging ? "5px solid pink" : "0px" }}
-    />
+    <Resizable 
+      defaultSize={{
+        width: 155, 
+        height: 155
+      }}
+      onResizeStop={ResizeCallback}
+    >
+      { isShowDelBtn && <RiDeleteBinLine size={'2rem'} style={{
+        position: "absolute",
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)'
+      }} /> }
+      <img
+        ref={drag}
+        src={url}
+        width="100%"
+        height="100%"
+        alt="abc"
+        style={{ border: isDragging ? "5px solid pink" : "0px" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      />
+      
+      
+    </Resizable>
   );
 }
 
